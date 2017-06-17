3a335b7cb29c5b24acf32bde8f74784e66f64aa4
张静波 <jingbo.zhang@cafintech.com>
2017-06-17 17:30:29 +0800

    adf ;

--- src/main/java/com/cafintech/framework/Application.java
+++ src/main/java/com/cafintech/framework/Application.java
@@ -2,13 +2,11 @@ package com.cafintech.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Bob on 12/20/16.
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/cafintech/framework/controllers/Example.java
+++ src/main/java/com/cafintech/framework/controllers/Example.java
@@ -2,6 +2,7 @@ package com.cafintech.framework.controllers;
 
 import com.cafintech.framework.services.ExampleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,10 @@ public class Example {
     @Autowired
     private ExampleService exampleService;
 
-    @RequestMapping("/{param}")
-    String home(@PathParam("param") String hello) {
-        return exampleService.helloWorld(hello);
+    @RequestMapping("/hello/{param}")
+    String home(@PathVariable String param) {
+        System.out.println("sdfsf");
+        return exampleService.helloWorld(param);
     }
 
 }
